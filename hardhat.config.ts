a2ba9f16ca9a6440dd7427406e8b72e9d4900b89
Vesa-Ville <v-v@equilibrium.co>
2022-04-06 08:09:18 +0300

    Added create-deployment task for updating local deployment files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,10 @@ import { task } from "hardhat/config"
 import mintJuice from "./hardhat/MintJuice.action"
 import fetchJuiceOwners from "./hardhat/FetchJuiceOwners.action"
 import deployJuicenet from "./hardhat/DeployJuicenet.action"
+import createDeployment from "./hardhat/CreateDeployment.action"
 
 task("airdrop", "Mints the Juice for receivers defined in 'premine.json'", mintJuice)
 task("fetch-airdrop", "Fetches the airdrop receivers and writes the 'premine.json'", fetchJuiceOwners)
-task("deploy-juicenet", "").addOptionalParam("logic", "Optional Logic address, deploys proxy if set", "").setAction(deployJuicenet)
+task("deploy-juicenet", "Deploys the Juicenet logic and proxy contract").addOptionalParam("logic", "Optional Logic address, deploys proxy if set", "").setAction(deployJuicenet)
+task("create-deployment").addParam("tx", "Deployment tx hash", "").addParam("proxy", "Proxy contract address", "").setAction(createDeployment)
 export default hardhatConfig
